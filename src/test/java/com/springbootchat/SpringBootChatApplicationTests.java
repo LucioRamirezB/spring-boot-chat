3d9a85909e7e2877b55b95e0a0342dce4cdbd185
Lucio-RamirezB <lucio.ramirez@coppel.com>
2024-01-05 13:23:01 -0600

    Se agrega archivos para el desarrollo del proyecto

---
+++ src/test/java/com/springbootchat/SpringBootChatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springbootchat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootChatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
